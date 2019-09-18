cfa8b7c2dcc393e6cd0d207ef9e6fb36cbe16e75
sidiousvic <victor6rene@gmail.com>
2019-09-18 20:38:49 +0900

    wr bubblesort.js

---
+++ bubblesort.js
@@ -0,0 +1,22 @@
+const nums = [0, 892, 6, 9999, 9536, 63, 7, 634, 7, 87, 976];
+
+function bubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (i = 0; i < arr.length; i++) {
+      let l = arr[i];
+      let r = arr[i + 1];
+      if (l > r) {
+        let t = arr[i];
+        arr[i] = r;
+        arr[i + 1] = t;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+}
+
+console.log(bubbleSort(nums));
+// [0, 6, 7, 7, 63, 87, 634, 892, 976, 9536, 9999];
